bb2e85851bfaa518f3ce989029d11722f08a7e16
Silencio <77356433+S1lencio@users.noreply.github.com>
2025-04-04 12:20:24 +0200

    visual studio

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import ctypes
+import os
+import urllib.request
 
 # Helper functions
 def set_path(new_path):
@@ -32,6 +34,8 @@ def main():
     install_chocolatey()
     install_cpp()
     install_openssl()
+    installer_path = download_visual_studio_installer()
+    install_visual_studio(installer_path)
 
 def install_chocolatey():
     # Check if Chocolatey is already installed
@@ -63,7 +67,10 @@ def install_chocolatey():
 def install_cpp():
     try:
         print("Installing Visual C++ Redistributables...")
+
+        # Install
         subprocess.check_call(["choco", "install", "-y", "vcredist2013", "vcredist140"])
+
         print("Visual C++ Redistributables installation complete.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to install Visual C++ Redistributables: {e}")
@@ -73,13 +80,51 @@ def install_cpp():
 def install_openssl():
     try:
         print("Installing OpenSSL...")
+
+        # Install
         subprocess.check_call(["choco", "install", "-y", "openssl", "--version 1.1.1.2100"])
+
+        # Set environment variable
+        openssl_conf_path = r"C:\Program Files\OpenSSL-Win64\bin\openssl.cfg"
+        subprocess.call(["setx", "/m", "OPENSSL_CONF", openssl_conf_path])
+
+        # Set PATH
         set_path(r'C:\Program Files\OpenSSL-Win64\bin')
+
         print("OpenSSL installation complete.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to install OpenSSL: {e}")
         input()
         sys.exit(1)
+
+def download_visual_studio_installer():
+    url = "https://aka.ms/vs/16/release/vs_community.exe"
+    installer_path = os.path.join(os.getenv("TEMP"), "vs_installer.exe")
+
+    # Download the installer
+    print(f"Downloading Visual Studio installer from {url}...")
+    urllib.request.urlretrieve(url, installer_path)
+    print(f"Downloaded Visual Studio installer to {installer_path}")
+    return installer_path
+
+def install_visual_studio(installer_path):
+    # Command to install Visual Studio with the Desktop Development with C++ workload
+    command = [
+        installer_path,
+        "--path cache=" + os.getenv("TEMP"),
+        "--add", "Microsoft.VisualStudio.Workload.NativeDesktop",
+        "--remove", "Microsoft.VisualStudio.Component.CMake"
+        "--includeRecommended",
+        "--quiet",  # Silent install
+        "--norestart",  # Don't restart after install
+        "--wait"  # Wait until installation is complete
+    ]
+
+    # Run the installer command
+    print("Installing Visual Studio...")
+    subprocess.call(command)
+    print("Visual Studio installation completed.")
+
 
 # Run the installation function
 main()
